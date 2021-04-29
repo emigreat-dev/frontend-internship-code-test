26a03e458fc122350f49c0387203f524e161064c
emilivmari <82753681+emilivmari@users.noreply.github.com>
2021-04-29 20:00:51 +0200

    Update to solution

--- longestString.js
+++ longestString.js
@@ -1,3 +1,8 @@
 // TODO: Write a function that accepts an array of strings. Return the longest string in the array.
 
-const longest = arr.reduce( (a, b) => a.length > b.length ? a : b, 0);
+function getLongestString(arr) {
+  if(arr.every(i => (typeof i === "string"))) {
+    return arr.reduce( (a, b) => a.length > b.length ? a : b, 0);
+  }
+  return "The array contains elements that are not strings";
+}
